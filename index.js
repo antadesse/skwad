04b1a0c59f626672f76dde15e38c8f5556f6887c
SCSFMAC6 <scsfmac6@Admins-MacBook-Air.local>
2018-04-12 16:36:04 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express= require("express")
+// const BodyParser = require('body-praser');
+const app= express();
+app.set('view engine' , "ejs");
+app.get("/", (req, res) => res.render("index"))
+
+app.listen(3000, () => console.log('LIVE on 3000'))
